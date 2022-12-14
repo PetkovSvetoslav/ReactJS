d4248e16362989e866f29b5b33514e15f26f821e
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-12-14 12:26:14 +0200

    Add files via upload

---
+++ 0.my-app/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer  from './Footer';
+import {Header} from './Header';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
