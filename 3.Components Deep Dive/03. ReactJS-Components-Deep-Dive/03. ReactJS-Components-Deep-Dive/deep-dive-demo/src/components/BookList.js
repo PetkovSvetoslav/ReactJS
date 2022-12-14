7e8a5728d4b234ef8830a2e6539b2808ba5d2a1c
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2022-12-14 12:31:28 +0200

    Add files via upload

---
+++ 3.Components Deep Dive/03. ReactJS-Components-Deep-Dive/03. ReactJS-Components-Deep-Dive/deep-dive-demo/src/components/BookList.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { Book } from "./Book";
+
+export const BookList = (props) => {
+    return (
+        <ul>
+            {/* {props.books.map(x => <Book {...x} />)} */}
+            {props.books.map((x, i) => <Book key={i} {...x} />)}
+       
+        </ul>
+    );
+}
